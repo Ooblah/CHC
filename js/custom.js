1c851d2198a1670ba035efd32cc19fa0d12d4e79
Ooblah <thewonderbird@hotmail.com>
2014-07-29 15:38:28 -0400

    progress bar

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,19 @@
 $(document).ready(function() {
-    $('.nav-tabs a').click(function(e) {
-    	e.preventDefault();
-    	$(this).tab('show');
-    });
+	$('.nav-tabs a').click(function(e) {
+		e.preventDefault();
+		$(this).tab('show');
+	});
+
+	$(document).ready(function () {
+
+		$('#bar-1').jqbar({
+			animationSpeed: 500,
+			label: 'Label',
+			value: 90,
+			barColor: '#D64747',
+			orientation: 'h',
+			barWidth: 8 
+		});
+
+	});
 });
