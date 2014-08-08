b748d5cb1d0a46931c53c48d3a54b3e8a713bcd4
Ooblah <thewonderbird@hotmail.com>
2014-08-08 14:39:19 -0400

    map images

--- js/custom.js
+++ js/custom.js
@@ -94,6 +94,66 @@ $(document).ready(function() {
 		barLength: 170
 	});
 
+	$('#bar-20').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 31,
+		barLength: 170
+	});
+
+	$('#bar-21').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 72,
+		barLength: 170
+	});
+
+	$('#bar-22').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 54,
+		barLength: 170
+	});
+
+	$('#bar-23').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 85,
+		barLength: 170
+	});
+
+	$('#bar-24').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 56,
+		barLength: 170
+	});
+
+	$('#bar-25').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 25,
+		barLength: 170
+	});
+
 	//large bars
 	$('#bar-10').jqbar({
 		animationSpeed: 500,
@@ -103,6 +163,97 @@ $(document).ready(function() {
 		label: 'Label',
 		value: 77,
 		barLength: 170
+	});
+
+	//mini bars
+	$('#bar-11').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 55,
+		barLength: 100
+	});
+
+	$('#bar-12').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 53,
+		barLength: 100
+	});
+
+	$('#bar-13').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 77,
+		barLength: 100
+	});
+
+	$('#bar-14').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 87,
+		barLength: 100
+	});
+
+	$('#bar-15').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 23,
+		barLength: 100
+	});
+
+	$('#bar-16').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 54,
+		barLength: 100
+	});
+
+	$('#bar-17').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 99,
+		barLength: 100
+	});
+
+	$('#bar-18').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 82,
+		barLength: 100
+	});
+
+	$('#bar-19').jqbar({
+		animationSpeed: 500,
+		barColor: '#0f6b99',
+		orientation: 'h',
+		barWidth: 8,
+		label: 'Label',
+		value: 79,
+		barLength: 100
 	});
 
 	$('.poll button').click(function(e) {
